08669a2d610c46bf11dba1638c540db22a8c62a4
khorevigor <ig.khorev@gmail.com>
2022-01-17 19:56:03 +0300

    Pull existing tracks list
    Extended Audio tracks repository and firebase services functionality
    to provide list of previously uploaded tracks

--- app/src/main/java/com/dsphoenix/soundvault/data/model/Track.kt
+++ app/src/main/java/com/dsphoenix/soundvault/data/model/Track.kt
@@ -3,8 +3,8 @@ package com.dsphoenix.soundvault.data.model
 import android.net.Uri
 
 data class Track (
-    val name: String,
+    val name: String? = null,
     val description: String? = null,
-    val uri: Uri,
+    val uri: Uri? = null,
     val remotePath: String? = null
     )
