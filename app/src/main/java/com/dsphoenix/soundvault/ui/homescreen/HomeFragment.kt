bea4ebdb0db578611f058e1a9e27cf00ac4bca64
khorevigor <ig.khorev@gmail.com>
2022-01-17 19:57:42 +0300

    Show list of existing tracks on Home screen
    Created basic Home screen view and ViewModel to display
    list of existing items in firestore

--- app/src/main/java/com/dsphoenix/soundvault/ui/homescreen/HomeFragment.kt
+++ app/src/main/java/com/dsphoenix/soundvault/ui/homescreen/HomeFragment.kt
@@ -1,12 +1,43 @@
 package com.dsphoenix.soundvault.ui.homescreen
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.dsphoenix.soundvault.R
+import com.dsphoenix.soundvault.databinding.HomeScreenFragmentLayoutBinding
 
 private const val TAG = "HomeFragment"
 
-class HomeFragment : Fragment(R.layout.home_screen_fragment_layout) {
+class HomeFragment : Fragment() {
+    private lateinit var viewModelFactory: HomeViewModelFactory
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        viewModelFactory = HomeViewModelFactory()
+        viewModel = ViewModelProvider(this, viewModelFactory).get(HomeViewModel::class.java)
+
+        val binding = DataBindingUtil.inflate<HomeScreenFragmentLayoutBinding>(
+            layoutInflater,
+            R.layout.home_screen_fragment_layout,
+            container,
+            false
+        ).apply {
+            viewmodel = viewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
+
+        return binding.root
+    }
+
     companion object {
-        val navigationTag = "HomeFragment"
+        const val navigationTag = "HomeFragment"
     }
 }
