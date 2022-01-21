bceae175935c0e27e60293e6069a44eafae4d456
Ihar Khorau <dostup.96@gmail.com>
2022-01-21 17:17:24 +0300

    Use View binding
    Data binding removed, View binding used instead

--- app/src/main/java/com/dsphoenix/soundvault/ui/homescreen/HomeFragment.kt
+++ app/src/main/java/com/dsphoenix/soundvault/ui/homescreen/HomeFragment.kt
@@ -4,18 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.dsphoenix.soundvault.R
 import com.dsphoenix.soundvault.databinding.HomeScreenFragmentLayoutBinding
+import com.dsphoenix.soundvault.databinding.UploadScreenFragmentLayoutBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 private const val TAG = "HomeFragment"
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
+    private var _binding: HomeScreenFragmentLayoutBinding? = null
+    private val binding get() = _binding!!
+
     private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
@@ -23,17 +26,23 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = DataBindingUtil.inflate<HomeScreenFragmentLayoutBinding>(
-            layoutInflater,
-            R.layout.home_screen_fragment_layout,
-            container,
-            false
-        ).apply {
-            viewmodel = viewModel
-            lifecycleOwner = viewLifecycleOwner
-        }
+        _binding = HomeScreenFragmentLayoutBinding.inflate(inflater, container, false)
+
+        setupView()
 
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setupView() {
+        binding.rvTracks.adapter = TracksAdapter()
+        viewModel.tracks.observe(viewLifecycleOwner) { tracks ->
+            (binding.rvTracks.adapter as TracksAdapter).setData(tracks)
+        }
     }
 
     companion object {
--- app/src/main/java/com/dsphoenix/soundvault/ui/uploadscreen/UploadFileFragment.kt
+++ app/src/main/java/com/dsphoenix/soundvault/ui/uploadscreen/UploadFileFragment.kt
@@ -5,40 +5,67 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.databinding.DataBindingUtil
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.dsphoenix.soundvault.R
-import com.dsphoenix.soundvault.databinding.UploadScreanFragmentLayoutBinding
+import com.dsphoenix.soundvault.databinding.UploadScreenFragmentLayoutBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 private const val TAG = "UploadFileFragment"
 
 @AndroidEntryPoint
 class UploadFileFragment : Fragment() {
+    private var _binding: UploadScreenFragmentLayoutBinding? = null
+    private val binding get() = _binding!!
+
     private val viewModel: UploadFileViewModel by viewModels()
+
+    private val activityLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {uri ->
+        viewModel.uri.value = uri
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val activityLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {uri ->
-            viewModel.uri.value = uri
-        }
+        _binding = UploadScreenFragmentLayoutBinding.inflate(inflater, container, false)
 
-        viewModel.openFileButtonClick = { activityLauncher.launch("audio/mpeg") }
-
-        val binding = DataBindingUtil.inflate<UploadScreanFragmentLayoutBinding>(
-            layoutInflater,
-            R.layout.upload_screan_fragment_layout,
-            container,
-            false
-        ).apply {
-            viewmodel = viewModel
-            lifecycleOwner = viewLifecycleOwner
-        }
+        setupView()
 
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setupView() {
+        binding.btnPickFile.setOnClickListener { onPickFileClick() }
+        binding.btnUpload.setOnClickListener { onUploadButtonClick() }
+        binding.btnUpload.isEnabled = false
+        viewModel.filename.observe(viewLifecycleOwner) { text ->
+            val editText = binding.etFileName
+            if (text != editText.text.toString())
+                editText.setText(text)
+        }
+        viewModel.isValid.observe(viewLifecycleOwner) {
+            binding.btnUpload.isEnabled = it
+        }
+        binding.etFileName.doAfterTextChanged {
+            viewModel.filename.value = it.toString()
+        }
+        binding.etFileDescription.doAfterTextChanged {
+            viewModel.description.value = it.toString()
+        }
+    }
+
+    private fun onPickFileClick() {
+        activityLauncher.launch("audio/mpeg")
+    }
+
+    private fun onUploadButtonClick() {
+        viewModel.uploadTrack()
     }
 }
