d09e008f03beb1034e6bbdbd0dc148f1dd57b090
khorevigor <ig.khorev@gmail.com>
2022-01-28 17:12:39 +0300

    Add base view binding fragment

--- app/src/main/java/com/dsphoenix/soundvault/ui/uploadscreen/UploadFileFragment.kt
+++ app/src/main/java/com/dsphoenix/soundvault/ui/uploadscreen/UploadFileFragment.kt
@@ -1,44 +1,26 @@
 package com.dsphoenix.soundvault.ui.uploadscreen
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.widget.doAfterTextChanged
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.dsphoenix.soundvault.databinding.UploadScreenFragmentLayoutBinding
+import com.dsphoenix.soundvault.utils.viewbinding.ViewBindingFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 private const val TAG = "UploadFileFragment"
 
 @AndroidEntryPoint
-class UploadFileFragment : Fragment() {
-    private var _binding: UploadScreenFragmentLayoutBinding? = null
-    private val binding get() = _binding!!
-
+class UploadFileFragment: ViewBindingFragment<UploadScreenFragmentLayoutBinding>(UploadScreenFragmentLayoutBinding::inflate) {
     private val viewModel: UploadFileViewModel by viewModels()
 
     private val activityLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {uri ->
         viewModel.uri.value = uri
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = UploadScreenFragmentLayoutBinding.inflate(inflater, container, false)
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setupView()
-
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 
     private fun setupView() {
