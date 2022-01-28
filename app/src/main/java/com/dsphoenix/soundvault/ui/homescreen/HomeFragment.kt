e84304af76a454fd27e93b92b1ea27e3f42d1df9
khorevigor <ig.khorev@gmail.com>
2022-01-28 17:12:39 +0300

    Add base view binding fragment

--- app/src/main/java/com/dsphoenix/soundvault/ui/homescreen/HomeFragment.kt
+++ app/src/main/java/com/dsphoenix/soundvault/ui/homescreen/HomeFragment.kt
@@ -1,41 +1,21 @@
 package com.dsphoenix.soundvault.ui.homescreen
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
-import com.dsphoenix.soundvault.R
 import com.dsphoenix.soundvault.databinding.HomeScreenFragmentLayoutBinding
-import com.dsphoenix.soundvault.databinding.UploadScreenFragmentLayoutBinding
+import com.dsphoenix.soundvault.utils.viewbinding.ViewBindingFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 private const val TAG = "HomeFragment"
 
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
-    private var _binding: HomeScreenFragmentLayoutBinding? = null
-    private val binding get() = _binding!!
+class HomeFragment : ViewBindingFragment<HomeScreenFragmentLayoutBinding>(HomeScreenFragmentLayoutBinding::inflate) {
 
     private val viewModel: HomeViewModel by viewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = HomeScreenFragmentLayoutBinding.inflate(inflater, container, false)
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setupView()
-
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 
     private fun setupView() {
