710b46ca0457852937819f719345d95beba0cf81
Ihar Khorau <dostup.96@gmail.com>
2022-02-16 02:12:29 +0300

    Create base view for track details/player

---
+++ app/src/main/java/com/dsphoenix/soundvault/ui/trackdetails/TrackDetailsFragment.kt
@@ -0,0 +1,7 @@
+package com.dsphoenix.soundvault.ui.trackdetails
+
+import com.dsphoenix.soundvault.databinding.TrackDetailsFragmentBinding
+import com.dsphoenix.soundvault.utils.viewbinding.ViewBindingFragment
+
+class TrackDetailsFragment: ViewBindingFragment<TrackDetailsFragmentBinding>(TrackDetailsFragmentBinding::inflate) {
+}
