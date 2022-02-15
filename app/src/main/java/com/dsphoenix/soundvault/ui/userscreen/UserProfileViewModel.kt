cfac7b66ff4227f44adbbed5002055ca11c8738a
Ihar Khorau <dostup.96@gmail.com>
2022-02-16 02:08:13 +0300

    Create base view for user profile

---
+++ app/src/main/java/com/dsphoenix/soundvault/ui/userscreen/UserProfileViewModel.kt
@@ -0,0 +1,10 @@
+package com.dsphoenix.soundvault.ui.userscreen
+
+import androidx.lifecycle.ViewModel
+import com.dsphoenix.soundvault.data.AudioRepository
+import javax.inject.Inject
+
+class UserProfileViewModel @Inject constructor(
+    audioRepository: AudioRepository
+) : ViewModel() {
+}
