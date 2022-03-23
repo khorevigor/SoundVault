6ff94b343ab1105d4f015513fcf3175477e46398
Ihar Khorau <dostup.96@gmail.com>
2022-03-23 15:07:04 +0300

    Create FadingImageView
    Created custom view to display image with faded bottom border

---
+++ app/src/main/java/com/dsphoenix/soundvault/utils/constants/Attributes.kt
@@ -0,0 +1,10 @@
+package com.dsphoenix.soundvault.utils.constants
+
+object Attributes {
+    object Schemas
+    {
+        const val ANDROID = "http://schemas.android.com/apk/res/android"
+    }
+
+    const val SRC = "src"
+}
---
+++ app/src/main/java/com/dsphoenix/soundvault/widgets/fadingImageView/FadingImageView.kt
@@ -0,0 +1,68 @@
+package com.dsphoenix.soundvault.widgets.fadingImageView
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.*
+import android.graphics.BitmapFactory.decodeResource
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.graphics.scale
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.dsphoenix.soundvault.utils.constants.Attributes
+
+// Since there is two similar LinearGradient constructors with difference that
+// API level 1 version doesn't throw exception, while API level 29 version does
+@SuppressLint("NewApi")
+
+class FadingImageView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : View(context, attrs) {
+
+    private val srcResource = attrs?.getAttributeResourceValue(Attributes.Schemas.ANDROID, Attributes.SRC, 0) as Int
+    private var bitmap: Bitmap = decodeResource(context.resources, srcResource)
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    // Opaque white to transparent
+    private val colors = intArrayOf(0xffffffff.toInt(), 0x55ffffff, 0x00ffffff)
+    private lateinit var gradientShader: Shader
+    private lateinit var bitmapShader: Shader
+
+    override fun onDraw(canvas: Canvas) {
+        if (bitmap.width != width || bitmap.height != height) {
+            setupShaders()
+        }
+        canvas.drawRect(0f, 0f, bitmap.width.toFloat(), bitmap.height.toFloat(), paint)
+    }
+
+    fun loadImage(context: Context, uri: String) {
+        Glide.with(context).asBitmap().load(uri)
+            .into(object : CustomTarget<Bitmap>(bitmap.width, bitmap.height){
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    bitmap = resource
+                    invalidate()
+                }
+                override fun onLoadCleared(placeholder: Drawable?) {
+                }
+            })
+    }
+
+    private fun setupShaders() {
+        bitmap = bitmap.scale(width, height)
+
+        gradientShader = LinearGradient(
+            (width * 0.875).toFloat(), height.toFloat(), width.toFloat(), height.toFloat(),
+            colors, null, Shader.TileMode.CLAMP
+        )
+
+        val shaderAMatrix = Matrix()
+        gradientShader.getLocalMatrix(shaderAMatrix)
+        shaderAMatrix.postRotate(90f)
+        bitmapShader = BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+        gradientShader.setLocalMatrix(shaderAMatrix)
+
+        paint.shader = ComposeShader(gradientShader, bitmapShader, PorterDuff.Mode.SRC_IN)
+    }
+}
