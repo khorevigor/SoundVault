fa6760131b16be326e9de49be7d43c848fee7ebb
Ihar Khorau <ig.khorev@gmail.com>
2022-04-14 15:01:30 +0300

    Add possibility to play list of tracks
    Media player now can be provided with list of tracks
    to play.
    Added random and looping playback modes
    
    Enabled all media related buttons on TrackDetails screen

--- app/src/main/java/com/dsphoenix/soundvault/utils/mediaplayer/MediaPlayer.kt
+++ app/src/main/java/com/dsphoenix/soundvault/utils/mediaplayer/MediaPlayer.kt
@@ -11,8 +11,6 @@ import com.dsphoenix.soundvault.utils.TAG
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.flatMapConcat
-import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.isActive
 import kotlin.math.roundToInt
@@ -21,20 +19,34 @@ class MediaPlayer {
     private val _isPlaying = MutableStateFlow<Boolean?>(null)
     val isPlaying = _isPlaying.asLiveData()
 
+    private val _isShuffled = MutableStateFlow(false)
+    val isShuffled = _isShuffled.asLiveData()
+
+    private val _isLooping = MutableStateFlow(false)
+    val isLooping = _isLooping.asLiveData()
+
     private val _trackCurrentPosition = flow {
         while (currentCoroutineContext().isActive) {
-            emit (mediaPlayer.currentPosition)
+            if (mediaPlayer.isPlaying) {
+                emit(mediaPlayer.currentPosition)
+            }
             delay(100)
         }
     }
+
     val trackCurrentPosition = _trackCurrentPosition.asLiveData()
+
+    private val _trackDuration = MutableLiveData<Int>()
+    val trackDuration: LiveData<Int> = _trackDuration
 
     val trackProgress = _trackCurrentPosition.asLiveData().map {
         ((it.toDouble() / mediaPlayer.duration) * 100).roundToInt()
     }
 
-    val duration
-        get() = mediaPlayer.duration
+    private var originalPlaybackList = emptyList<Track>()
+    private var playbackList = emptyList<Track>()
+
+    private var currentIndex: Int = -1
 
     private val _currentTrack = MutableLiveData<Track>()
     val currentTrack: LiveData<Track> = _currentTrack
@@ -44,11 +56,13 @@ class MediaPlayer {
     private val mediaPlayer = MediaPlayer().apply {
         setOnPreparedListener {
             start()
+            _trackDuration.value = this.duration
             _isPlaying.value = it.isPlaying
         }
         setOnCompletionListener {
+            Log.d(TAG, "onCompletionListenerCalled")
             _isPlaying.value = it.isPlaying
-            onCompletionCallback?.invoke()
+            playNext()
         }
     }
 
@@ -61,6 +75,18 @@ class MediaPlayer {
                 prepareAsync()
             }
         }
+        if (playbackList.isNotEmpty()) {
+            updateCurrentIndex()
+        }
+    }
+
+    fun setTracksQueue(tracks: List<Track>) {
+        originalPlaybackList = tracks
+        playbackList = tracks
+    }
+
+    private fun updateCurrentIndex() {
+        currentIndex = playbackList.indexOf(_currentTrack.value)
     }
 
     fun pause() {
@@ -73,5 +99,70 @@ class MediaPlayer {
         Log.d(TAG, "resume called")
         mediaPlayer.start()
         _isPlaying.value = mediaPlayer.isPlaying
+    }
+
+    fun playNext() {
+        mediaPlayer.stop()
+        setTrackToPlay(next())
+    }
+
+    fun playPrev() {
+        if (mediaPlayer.currentPosition < 2000) {
+            setTrackToPlay(prev())
+        }
+        else {
+            playCurrentFromStart()
+        }
+    }
+
+    fun toggleShuffle() {
+        if (_isShuffled.value) {
+            playbackList = originalPlaybackList
+            updateCurrentIndex()
+        }
+        else {
+            playbackList = playbackList.shuffled()
+            updateCurrentIndex()
+        }
+        _isShuffled.apply { value = !value }
+    }
+
+    fun toggleLooping() {
+        mediaPlayer.isLooping = !mediaPlayer.isLooping
+        _isLooping.value = mediaPlayer.isLooping
+    }
+
+    private fun playCurrentFromStart() {
+        mediaPlayer.seekTo(0)
+    }
+
+    private fun getFirstOrCurrent(): Track {
+        val track = _currentTrack.value
+        return if (playbackList.isEmpty() && track != null) {
+            currentIndex = -1
+            track
+        } else {
+            currentIndex = 0
+            playbackList.first()
+        }
+    }
+
+    private fun getLastOrCurrent(): Track {
+        val track = _currentTrack.value
+        return if (playbackList.isEmpty() && track != null) {
+            currentIndex = -1
+            track
+        } else {
+            currentIndex = playbackList.lastIndex
+            playbackList.last()
+        }
+    }
+
+    private fun next(): Track {
+        return playbackList.getOrElse(++currentIndex) { getFirstOrCurrent() }
+    }
+
+    private fun prev(): Track {
+        return playbackList.getOrElse(--currentIndex) { getLastOrCurrent() }
     }
 }
